82824f55c21385a9a5a78dc98dd0afddda3af4a9
carnero <carnero@carnero.cc>
2013-01-05 17:55:21 +0100

    Init

---
+++ src/carnero/me/Constants.java
@@ -0,0 +1,6 @@
+package carnero.me;
+
+public class Constants {
+
+	public static final String ID_LATITUDE = "-4501092395013557129";
+}
---
+++ src/carnero/me/activity/Main.java
@@ -0,0 +1,14 @@
+package carnero.me.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import carnero.me.R;
+
+public class Main extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.main);
+	}
+}
