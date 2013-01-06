894f54633c19f9e58ddb89e130dc20d7364d7b8c
carnero <carnero@carnero.cc>
2013-01-06 15:43:14 +0100

    - Location

--- src/carnero/me/Constants.java
+++ src/carnero/me/Constants.java
@@ -3,4 +3,11 @@ package carnero.me;
 public class Constants {
 
 	public static final String ID_LATITUDE = "-4501092395013557129"; // to manage public location visit https://www.google.com/latitude/apps/badge
+
+	public static final int MAP_SQUARE_WIDTH = 80;
+	public static final int MAP_SQUARE_HEIGHT = 42;
+	public static final int MAP_LATITUDE_MIN_E6 = (int) (-164 * 1e6); // left
+	public static final int MAP_LATITUDE_MAX_E6 = (int) (+164 * 1e6); // right
+	public static final int MAP_LONGITUDE_MIN_E6 = (int) (-56 * 1e6); // bottom
+	public static final int MAP_LONGITUDE_MAX_E6 = (int) (+80 * 1e6); // top
 }
--- src/carnero/me/activity/Main.java
+++ src/carnero/me/activity/Main.java
@@ -5,24 +5,25 @@ import android.gesture.GestureOverlayView;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.widget.TextView;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.*;
+import carnero.me.Constants;
 import carnero.me.R;
 import carnero.me.network.Latitude;
 import com.google.android.maps.GeoPoint;
 
 public class Main extends Activity {
 
-	private TextView mDebug;
 	private int mLatitude;
 	private int mLongitude;
-	private Handler mHandler = new Handler() {
-
-		public void handleMessage(Message message) {
-			if (mDebug != null) {
-				mDebug.setText(String.valueOf(mLatitude) + "/" + String.valueOf(mLongitude));
-			}
-		}
-	};
+	// views
+	private ViewPager mPager;
+	private ImageView mMap;
+	private View mLocation;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -30,18 +31,83 @@ public class Main extends Activity {
 
 		setContentView(R.layout.main);
 
-		mDebug = (TextView) findViewById(R.id.debug);
+		mPager = (ViewPager) findViewById(R.id.pager);
+		mMap = (ImageView) findViewById(R.id.location_map);
+		mLocation = findViewById(R.id.location_current);
+	}
 
-		new Thread() {
+	@Override
+	public void onResume() {
+		super.onResume();
 
-			@Override
-			public void run() {
-				final GeoPoint gp = new Latitude().getCoordinates();
-				mLatitude = gp.getLatitudeE6();
-				mLongitude = gp.getLongitudeE6();
+		getLocation();
+	}
 
-				mHandler.sendEmptyMessage(1);
+	private void getLocation() {
+		final LocationHandler handler = new LocationHandler();
+		final LocationThread thread = new LocationThread(handler);
+
+		thread.start();
+	}
+
+	// classes
+	private class LocationThread extends Thread {
+
+		private Handler mHandler;
+
+		public LocationThread(Handler handler) {
+			mHandler = handler;
+		}
+
+		@Override
+		public void run() {
+			final GeoPoint gp = new Latitude().getCoordinates();
+			mLatitude = gp.getLatitudeE6();
+			mLongitude = gp.getLongitudeE6();
+
+			mHandler.sendEmptyMessage(1);
+		}
+	}
+
+	private class LocationHandler extends Handler {
+
+		public void handleMessage(Message message) {
+			// whole map
+			final int mapWidth = mMap.getWidth();
+			final int mapHeight = mMap.getHeight();
+			// pixel square
+			final int squareSize = Math.round(((mapWidth / 80) + (mapHeight / 42)) / 2);
+			final int squareBorder = squareSize / 8;
+			// map square
+			final int mapSquareLat = (Constants.MAP_LATITUDE_MAX_E6 - Constants.MAP_LATITUDE_MIN_E6) / Constants.MAP_SQUARE_WIDTH;
+			final int mapSquareLon = (Constants.MAP_LONGITUDE_MAX_E6 - Constants.MAP_LONGITUDE_MIN_E6) / Constants.MAP_SQUARE_HEIGHT;
+			// map border
+			final int mapLatitudeMargin = (int) Math.abs((-180 * 1e6) - Constants.MAP_LATITUDE_MIN_E6);
+			final int mapLongitudeMargin = (int) Math.abs((90 * 1e6) - Constants.MAP_LONGITUDE_MAX_E6);
+
+			// map bounds
+			if (mLatitude < Constants.MAP_LATITUDE_MIN_E6) {
+				mLatitude = Constants.MAP_LATITUDE_MIN_E6;
+			} else if (mLatitude > Constants.MAP_LATITUDE_MAX_E6) {
+				mLatitude = Constants.MAP_LATITUDE_MAX_E6;
 			}
-		}.start();
+			if (mLongitude < Constants.MAP_LONGITUDE_MIN_E6) {
+				mLongitude = Constants.MAP_LONGITUDE_MIN_E6;
+			} else if (mLongitude > Constants.MAP_LONGITUDE_MAX_E6) {
+				mLongitude = Constants.MAP_LONGITUDE_MAX_E6;
+			}
+
+			// location pixel margin
+			final int leftPos = (mLatitude - mapLatitudeMargin) - Constants.MAP_LATITUDE_MIN_E6;
+			final int topPos = Constants.MAP_LONGITUDE_MAX_E6 - (mLongitude - mapLongitudeMargin);
+			final int left = (leftPos / mapSquareLat) * squareSize - squareBorder;
+			final int top = (topPos / mapSquareLon) * squareSize - squareBorder;
+
+			FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+			params.setMargins(left, top, 0, 0);
+
+			mLocation.setLayoutParams(params);
+			mLocation.setVisibility(View.VISIBLE);
+		}
 	}
 }
