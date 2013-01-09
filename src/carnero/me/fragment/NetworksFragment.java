a0b62f31037e81f2404f806fde20e580a2b08fe0
carnero <carnero@carnero.cc>
2013-01-09 17:14:29 +0100

    Official client check

--- src/carnero/me/fragment/NetworksFragment.java
+++ src/carnero/me/fragment/NetworksFragment.java
@@ -3,6 +3,8 @@ package carnero.me.fragment;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -10,15 +12,21 @@ import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import carnero.me.R;
 
 public class NetworksFragment extends Fragment {
 
 	private View mContent;
+	private LinearLayout mLayout;
+	private int mOrdered = 0;
+	private PackageManager mPM;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedState) {
 		mContent  = inflater.inflate(R.layout.networks, container, false);
+
+		mLayout = (LinearLayout) mContent.findViewById(R.id.networks_layout);
 
 		return mContent;
 	}
@@ -27,14 +35,46 @@ public class NetworksFragment extends Fragment {
 	public void onActivityCreated(Bundle savedState) {
 		super.onActivityCreated(savedState);
 
-		mContent.findViewById(R.id.linkedin_layout).setOnClickListener(new Launcher(getActivity(), "http://www.linkedin.com/in/carnerocc/"));
-		mContent.findViewById(R.id.google_plus_layout).setOnClickListener(new Launcher(getActivity(), "http://plus.google.com/116645889171150251778/posts"));
-		mContent.findViewById(R.id.twitter_layout).setOnClickListener(new Launcher(getActivity(), "https://twitter.com/carnero_cc"));
-		mContent.findViewById(R.id.instagram_layout).setOnClickListener(new Launcher(getActivity(), "http://instagram.com/_carnero/"));
-		mContent.findViewById(R.id.flickr_layout).setOnClickListener(new Launcher(getActivity(), "http://www.flickr.com/photos/carnero/"));
-		mContent.findViewById(R.id.fivehundred_layout).setOnClickListener(new Launcher(getActivity(), "http://500px.com/carnero/photos"));
-		mContent.findViewById(R.id.pinterest_layout).setOnClickListener(new Launcher(getActivity(), "http://pinterest.com/carnero/"));
-		mContent.findViewById(R.id.foursquare_layout).setOnClickListener(new Launcher(getActivity(), "http://foursquare.com/carnero_cc"));
+		mOrdered = 0;
+		resort(mContent.findViewById(R.id.linkedin_layout), "com.linkedin.android").setOnClickListener(new Launcher(getActivity(), "http://www.linkedin.com/in/carnerocc/"));
+		resort(mContent.findViewById(R.id.google_plus_layout), "com.google.android.apps.plus").setOnClickListener(new Launcher(getActivity(), "http://plus.google.com/116645889171150251778/posts"));
+		resort(mContent.findViewById(R.id.twitter_layout), "com.twitter.android").setOnClickListener(new Launcher(getActivity(), "https://twitter.com/carnero_cc"));
+		resort(mContent.findViewById(R.id.instagram_layout), "com.instagram.android").setOnClickListener(new Launcher(getActivity(), "http://instagram.com/_carnero/"));
+		resort(mContent.findViewById(R.id.flickr_layout), "com.yahoo.mobile.client.android.flickr").setOnClickListener(new Launcher(getActivity(), "http://www.flickr.com/photos/carnero/"));
+		resort(mContent.findViewById(R.id.fivehundred_layout), "com.fivehundredpx.viewer").setOnClickListener(new Launcher(getActivity(), "http://500px.com/carnero/photos"));
+		resort(mContent.findViewById(R.id.pinterest_layout), "com.pinterest").setOnClickListener(new Launcher(getActivity(), "http://pinterest.com/carnero/"));
+		resort(mContent.findViewById(R.id.foursquare_layout), "com.joelapenna.foursquared").setOnClickListener(new Launcher(getActivity(), "http://foursquare.com/carnero_cc"));
+	}
+
+	private View resort(View view, String packageName) {
+		if (isPackageInstalled(packageName)) {
+			mLayout.removeView(view);
+			mLayout.addView(view, mOrdered);
+
+			mOrdered ++;
+		} else {
+			view.setAlpha(0.6f);
+		}
+
+		return view;
+	}
+
+	private boolean isPackageInstalled(String clazz) {
+		if (TextUtils.isEmpty(clazz)) {
+			return false;
+		}
+
+		if (mPM == null) {
+			mPM = getActivity().getPackageManager();
+		}
+
+		try {
+			mPM.getPackageInfo(clazz, 0);
+
+			return true;
+		} catch (NameNotFoundException e) {
+			return false;
+		}
 	}
 
 	private class Launcher implements View.OnClickListener {
